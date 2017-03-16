aef5685f71ad973a9ed6e3a02651d758567fae0c
Griffin <tabor473@gmail.com>
2017-03-16 17:25:33 -0400

    Hello World moved in

---
+++ Hello_World/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Griffin on 3/16/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    System.out.println("test");
+    }
+
+
+}
+
