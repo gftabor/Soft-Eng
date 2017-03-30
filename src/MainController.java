43dd19fbfc313de6552848a419e1f177d21513ac
Jason Ashton <jlashton@wpi.edu>
2017-03-30 19:43:29 -0400

    Set up MainController, added simple println to verify that it is working

---
+++ src/MainController.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jasonashton on 3/30/17.
+ */
+public class MainController {
+    public static void main (String[] args){
+        System.out.println("This is working");
+    }
+}
