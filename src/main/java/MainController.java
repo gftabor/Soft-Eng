4e4594edc0b49eadecc28c81a67b1d549eef5042
Griffin <tabor473@gmail.com>
2017-04-01 17:29:26 -0400

    Code organization. Deleted duplicate classes

--- src/main/java/MainController.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jasonashton on 3/30/17.
- */
-public class MainController {
-    public static void main (String[] args){
-        System.out.println("This is working");
-    }
-}
