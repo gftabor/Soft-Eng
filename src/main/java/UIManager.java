1f9934c697c30cc0a691cb93581aeee227a3df34
Griffin Cecil <gdcecil@wpi.edu>
2017-04-01 13:15:29 -0400

    Added shell of class UIManager

---
+++ src/main/java/UIManager.java
@@ -0,0 +1,28 @@
+/**
+ * Created by griffincecil on 4/1/17.
+ */
+
+public class UIManager {
+
+
+
+    public int displayMainMenu() {
+        return 0;
+    }
+
+    public int displayPathfinder() {
+        return 0;
+    }
+
+    public int displayDirectory() {
+        return 0;
+    }
+
+    public int displayMapManagement() {
+        return 0;
+    }
+
+    public int checkAdminPowers() {
+        return 0;
+    }
+}
