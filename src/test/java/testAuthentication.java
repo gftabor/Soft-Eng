cec3db2f01f38cda6b390e29b44a035ce14dc9ab
Jason Ashton <jlashton@wpi.edu>
2017-04-02 16:45:09 -0400

    Added jbcrypt to build Gradle, and edited Admin Login Manager to check the hash. Also added some more test cases for authentication

--- src/test/java/testAuthentication.java
+++ src/test/java/testAuthentication.java
@@ -8,9 +8,20 @@ import adminLoginMain.AdminLoginManager;
 public class testAuthentication {
 
     @Test
-    public void testAdminLogin(){
+    public void testAdminLoginCorrect(){
         AdminLoginManager loginManage = new AdminLoginManager();
         assertEquals(loginManage.verifyCredentials("Griffin", "1234"), 1);
     }
 
+    @Test
+    public void testAdminLoginWrongUsername(){
+        AdminLoginManager loginManage = new AdminLoginManager();
+        assertEquals(loginManage.verifyCredentials("XXX", "1234"), 0);
+    }
+
+    @Test
+    public void testAdminLoginWrongPassword(){
+        AdminLoginManager loginManage = new AdminLoginManager();
+        assertEquals(loginManage.verifyCredentials("Griffin", "0000"), 0);
+    }
 }
