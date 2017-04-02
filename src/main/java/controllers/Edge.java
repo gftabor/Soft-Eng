1b27c41ec7c73386ad90c365072bdff2f1d9c6ce
Ryan Walsh <rpwalsh@wpi.edu>
2017-04-02 16:06:37 -0400

    Filled out class implementaitons of Nodes and edges. Implemented distance algorithm for calculating edge weights.

--- src/main/java/controllers/Edge.java
+++ src/main/java/controllers/Edge.java
@@ -1,19 +1,23 @@
 package controllers;
 
+import java.lang.Math;
+
 /**
  * Created by mylena on 4/1/17.
  */
 public class Edge {
-    private String type;
-    private float weight;
-    public Node startNode;
-    public Node endNode;
+    private double weight;
+    private int floor;
+    private Node startNode;
+    private Node endNode;
 
-    public Edge(String type, Node startNode, Node endNode){
-        this.type = type;
+    //standard constructor
+    //  -input all fields at constructor
+    public Edge(Node startNode, Node endNode, int floor){
         this.startNode = startNode;
         this.endNode = endNode;
-        float weight = calculateWeight();
+        float weight = calculateWeight(startNode.getPosX(), startNode.getPosY(), endNode.getPosX(), endNode.getPosY());
+        this.floor = floor;
     }
 
     public void addEdge() {
@@ -24,8 +28,31 @@ public class Edge {
         System.out.println("Edge has been deleted");
     }
 
-    public float calculateWeight() {
-        return 0;
+    //use distance formula to calculate weight of edge (weight is used in pathfinding)
+    public double calculateWeight(int x1, int y1, int x2, int y2) {
+        //implementation of this formula:
+
+        //differences between coords
+        int xDiff;
+        int yDiff;
+
+        xDiff = x2 - x1;
+        yDiff = y2 - y1;
+
+        //square the differences
+        double squareX;
+        double squareY;
+        squareX = Math.pow(xDiff, 2);
+        squareY = Math.pow(yDiff, 2);
+
+        //sum the squares
+        double sumSquares;
+        sumSquares = squareX + squareY;
+        //square root for result
+        double result;
+        result = Math.sqrt(sumSquares);
+        return result;
+
     }
 
 }
--- src/main/java/controllers/Node.java
+++ src/main/java/controllers/Node.java
@@ -8,27 +8,56 @@ import java.util.ArrayList;
 public class Node {
     private int posX;
     private int posY;
-    private boolean hidden;
+    private boolean isHidden;
+    private boolean isEnabled;
     private String name;
+    private int floor;
 
     private ArrayList<Edge> edges = new ArrayList<Edge>();
 
-    public Node(int posX, int posY, boolean hidden, String name) {
+    //Basic Constructor
+    //  - must fill in all fields manually
+    public Node(int posX, int posY, boolean hidden, boolean enabled, String name, int floor) {
         this.posX = posX;
         this.posY = posY;
-        this.hidden = hidden;
+        this.isHidden = hidden;
+        this.isEnabled = enabled;
         this.name = name;
+        this.floor = floor;
+    }
+
+    //Quick Constructor
+    //  -assume enabled by default
+    public Node(int posX, int posY, boolean hidden, String name, int floor) {
+        this.posX = posX;
+        this.posY = posY;
+        this.isHidden = hidden;
+        this.isEnabled = true;
+        this.name = name;
+        this.floor = floor;
     }
 
     public String toString() {
         System.out.println("Node:");
         System.out.println("    Pos X: " + posX);
         System.out.println("    Pos y: " + posY);
-        System.out.println("    Hidden: " + hidden);
+        System.out.println("    Hidden: " + isHidden);
+        System.out.println("    Enabled: " + isEnabled);
         System.out.println("    Name: " + name);
 
         return null;
     }
 
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
 
 }
