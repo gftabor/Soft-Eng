baf61116a84278c0dfb1581320a7bf1d822bf394
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-02 13:17:55 -0400

    added a map management adding node controller and fix mmAddingNodeView

---
+++ src/main/java/mmAddingNodeController.java
@@ -0,0 +1,12 @@
+package main.java;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Created by AugustoR on 4/2/17.
+ */
+public class mmAddingNodeController {
+
+
+}
