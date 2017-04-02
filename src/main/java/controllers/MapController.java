b19462904f301cde73b13ba82576caa9c82c511f
dgiangrave <dlgiangrave@wpi.edu>
2017-04-02 17:45:41 -0400

    Add function for mapConroller requesting a map copy

--- src/main/java/controllers/MapController.java
+++ src/main/java/controllers/MapController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.sql.*;
+import java.sql.SQLException;
+
 //import main.java.controllers.CollectionOfNodes;
 
 /**
@@ -7,14 +10,37 @@ package controllers;
  */
 public class MapController {
 
-    private CollectionOfNodes mapCopy;
+    private CollectionOfNodes collectionOfNodes;
 
     MapController () {
-        mapCopy = new CollectionOfNodes();
+        collectionOfNodes = new CollectionOfNodes();
     }
 
 
-    private void requestMapCopy() {
+    private void requestMapCopy(ResultSet nodeRset) {
+        collectionOfNodes = new CollectionOfNodes();
+
+        try {
+            int x, y, floor;
+            boolean hidden;
+            String name;
+            Node node;
+            CollectionOfNodes collectionOfNodes = new CollectionOfNodes();
+            while (nodeRset.next()) {
+                x = nodeRset.getInt("XPOS");
+                y = nodeRset.getInt("YPOS");
+                hidden = nodeRset.getBoolean("HIDDEN?");
+                name = nodeRset.getString("NAME");
+                floor = nodeRset.getInt("FLOOR");
+                node = new Node(x, y, hidden, true, name, floor);
+                collectionOfNodes.addNode(node);
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
