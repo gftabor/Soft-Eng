b4246f873cc701c049a2dc694b7f538286f85a69
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-03 20:52:20 -0400

    Fixed the hospital directory and added the tree view properties

--- src/main/java/mapManagementNodeInformation/mmNodeInformationController.java
+++ src/main/java/mapManagementNodeInformation/mmNodeInformationController.java
@@ -2,12 +2,11 @@ package mapManagementNodeInformation;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;  //NOT JAVA SWING
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.TreeView;
 
 
 
@@ -19,34 +18,19 @@ public class mmNodeInformationController extends controllers.AbsController{
     private AnchorPane backgroundAnchorPane;
 
     @FXML
-    private Label currentMode_Label;
-
-    @FXML
     private Label currentAdmin_Label;
 
     @FXML
-    private Button signOut_Button;
+    private Button mainMenu_Button;
 
     @FXML
     private Button emergency_Button;
 
     @FXML
-    private TextField title_TextField;
-
-    @FXML
     private TextField name_TextField;
 
     @FXML
-    private TextField department_TextField;
-
-    @FXML
-    private TextField location_TextField;
-
-    @FXML
-    private TextField availability_TextField;
-
-    @FXML
-    private TextField contactInfo_TextField;
+    private TextField room_TextField;
 
     @FXML
     private Button cancel_Button;
@@ -55,7 +39,11 @@ public class mmNodeInformationController extends controllers.AbsController{
     private Button submit_Button;
 
     @FXML
-    private ScrollPane directory_ScrollPane;
+    private TreeView<String> directory_TreeView;
+
+
+
+
 
     public void cancelButton_Clicked(){
         System.out.println("The user has clicked the cancel Button");
@@ -71,10 +59,51 @@ public class mmNodeInformationController extends controllers.AbsController{
         switch_screen(backgroundAnchorPane, "/views/emergencyView.fxml");
     }
 
-    public void signOutButton_Clicked(){
+
+    public void mainMenuButton_Clicked(){
         System.out.println("The user has clicked the sign out Button");
+        switch_screen(backgroundAnchorPane, "/views/adminMenuStartView.fxml");
 
     }
+
+    //Creates the directory of the tree view
+    public void createDirectoryTreeView(){
+        TreeItem<String> root,doctors,nurses;
+
+        root = new TreeItem<>("List of Directories");
+        root.setExpanded(true);
+
+        doctors = makeBranch("Doctor's", root);
+        makeBranch("Doctor A", doctors);
+        makeBranch("Doctor B", doctors);
+        makeBranch("Doctor C", doctors);
+        doctors.setExpanded(false);
+
+        nurses = makeBranch("Nurses'", root);
+        makeBranch("Nurse A", nurses);
+        makeBranch("Nurse B", nurses);
+        nurses.setExpanded(false);
+
+        directory_TreeView.setRoot(root);
+
+        directory_TreeView.getSelectionModel().selectedItemProperty()
+                .addListener((v, oldValue, newValue) ->{
+                    if(newValue != null){
+                        System.out.println(newValue.getValue());
+                    }
+                });
+
+
+    }
+
+    //Create branches
+    public TreeItem<String> makeBranch(String title, TreeItem<String> parent){
+        TreeItem<String> item = new TreeItem<>(title);
+        item.setExpanded(true);
+        parent.getChildren().add(item);
+        return item;
+    }
+
 
 
 }
