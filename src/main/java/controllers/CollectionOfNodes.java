9a42ebc8c5636c85e0fde649d9eb751440338efb
Mylena Guzman <mylena.guzman@gmail.com>
2017-04-03 20:13:40 -0400

    changed CollectionOfNodes to now have a hashmap for each floor of nodes, and fixed methods (except for toString) accordingly

--- src/main/java/controllers/CollectionOfNodes.java
+++ src/main/java/controllers/CollectionOfNodes.java
@@ -9,28 +9,107 @@ import java.util.*;
 public class CollectionOfNodes {
 
     /*
-    KEY: Floor-Xpos-Ypos
-    Floor one digit, Xpos 4 digits, Ypos 4 digits
-    Example: 401500220 --> 4th floor, Xpos = 150, Ypos = 220
+    KEY: Xpos-Ypos
+    Xpos 4 digits, Ypos 4 digits
+    Example: 01500220 --> Xpos = 150, Ypos = 220
     */
 
-    private HashMap<Integer, Node> nodes;
+    private HashMap<Integer, Node> floor1nodes;
+    private HashMap<Integer, Node> floor2nodes;
+    private HashMap<Integer, Node> floor3nodes;
+    private HashMap<Integer, Node> floor4nodes;
+    private HashMap<Integer, Node> floor5nodes;
+    private HashMap<Integer, Node> floor6nodes;
+    private HashMap<Integer, Node> floor7nodes;
 
     public CollectionOfNodes() {
-        nodes = new HashMap<>();
+        floor1nodes = new HashMap<>();
+        floor2nodes = new HashMap<>();
+        floor3nodes = new HashMap<>();
+        floor4nodes = new HashMap<>();
+        floor5nodes = new HashMap<>();
+        floor6nodes = new HashMap<>();
+        floor7nodes = new HashMap<>();
     }
 
     public void addNode(Node node) {
         // Add node entry to the hashmap
-        nodes.put(node.getKey(), node);
+        int floor = node.getFloor();
+
+        switch(floor) {
+            case 1:
+                floor1nodes.put(node.getKey(), node);
+                break;
+
+            case 2:
+                floor2nodes.put(node.getKey(), node);
+                break;
+
+            case 3:
+                floor3nodes.put(node.getKey(), node);
+                break;
+
+            case 4:
+                floor4nodes.put(node.getKey(), node);
+                break;
+
+            case 5:
+                floor5nodes.put(node.getKey(), node);
+                break;
+
+            case 6:
+                floor6nodes.put(node.getKey(), node);
+                break;
+
+            case 7:
+                floor7nodes.put(node.getKey(), node);
+                break;
+            default:
+                System.out.println("CollectionOfNodes.addNode: Invalid floor");
+                break;
+        }
+
     }
 
     public void removeNode(Node node) {
         // Remove node entry from hashmap
-        nodes.remove(node.getKey());
+        int floor = node.getFloor();
+
+        switch(floor) {
+            case 1:
+                floor1nodes.remove(node.getKey());
+                break;
+
+            case 2:
+                floor2nodes.remove(node.getKey());
+                break;
+
+            case 3:
+                floor3nodes.remove(node.getKey());
+                break;
+
+            case 4:
+                floor4nodes.remove(node.getKey());
+                break;
+
+            case 5:
+                floor5nodes.remove(node.getKey());
+                break;
+
+            case 6:
+                floor6nodes.remove(node.getKey());
+                break;
+
+            case 7:
+                floor7nodes.remove(node.getKey());
+                break;
+            default:
+                System.out.println("CollectionOfNodes.addNode: Invalid floor");
+                break;
+        }
     }
 
-
+/*
     public String toString() {
         System.out.println("---------- Printing all nodes in CollectionOfNodes ----------");
 
@@ -44,8 +123,9 @@ public class CollectionOfNodes {
         System.out.println("-------------------------------------------------------------");
         return null;
     }
-
+*/
     public Node getNode(int x, int y, int floor) {
+        /*
         Node node;
         int key = generateNodeKey(x, y, floor);
 
@@ -53,15 +133,65 @@ public class CollectionOfNodes {
             node = nodes.get(key);
             return node;
         } else { return null; }
+        */
+        Node node;
+        int key = generateNodeKey(x, y);
+
+        switch(floor) {
+            case 1:
+                if(floor1nodes.containsKey(key)) {
+                    node = floor1nodes.get(key);
+                    return node;
+                } else { return null; }
+
+            case 2:
+                if(floor2nodes.containsKey(key)) {
+                    node = floor2nodes.get(key);
+                    return node;
+                } else { return null; }
+
+            case 3:
+                if(floor3nodes.containsKey(key)) {
+                    node = floor3nodes.get(key);
+                    return node;
+                } else { return null; }
+
+            case 4:
+                if(floor4nodes.containsKey(key)) {
+                    node = floor4nodes.get(key);
+                    return node;
+                } else { return null; }
+
+            case 5:
+                if(floor5nodes.containsKey(key)) {
+                    node = floor5nodes.get(key);
+                    return node;
+                } else { return null; }
+
+            case 6:
+                if(floor6nodes.containsKey(key)) {
+                    node = floor6nodes.get(key);
+                    return node;
+                } else { return null; }
+
+            case 7:
+                if(floor7nodes.containsKey(key)) {
+                    node = floor7nodes.get(key);
+                    return node;
+                } else { return null; }
+
+            default:
+                System.out.println("CollectionOfNodes.getNode: Invalid location");
+                return null;
+
+        }
+
     }
 
-    private int generateNodeKey(int x, int y, int floor) {
-        // Key in format [FXXXXYYYY]
-        // where F is floor, x's is the x cord, and y's are y cord
+    private int generateNodeKey(int x, int y) {
+        // Key in format [XXXXYYYY]
+        // where x's is the x cord, and y's are y cord
         int key = 0;
-
-        // Add nodes floor to key
-        key +=  floor * 100000000;
 
         // Add nodes x cord to key
         key +=  x * 10000;
