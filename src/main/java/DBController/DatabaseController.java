07658838090027e839122b00ff407f683d4b03d8
Jason Ashton <jlashton@wpi.edu>
2017-04-03 21:27:05 -0400

    Added edit, update, delete for tables node, edge, and professionals

--- src/main/java/DBController/DatabaseController.java
+++ src/main/java/DBController/DatabaseController.java
@@ -15,7 +15,6 @@ public class DatabaseController {
 
     Connection conn;
 
-
     private DatabaseController() {}
 
     public static DatabaseController getInstance() {
@@ -72,148 +71,189 @@ public class DatabaseController {
 
     }
 
-    /*
-=======
->>>>>>> .merge_file_a07212
-    public ArrayList<Node> getNodesInFloor(int floor){
-        String sqlString = "Select XPOS, YPOS, ISHIDDEN, NAME FROM NODE WHERE FLOOR = " + floor;
-        ArrayList<Node> nodes = new ArrayList<>();
-        int xPos;
-        int yPos;
-        String hiddenString;
-        boolean hidden;
-        String name;
+    public ResultSet getTableSet(String table){
+        String sqlString = "Select * FROM " + table;
+
+        ResultSet resultSet = null;
         try {
             Statement stmt = conn.createStatement();
-            ResultSet rset = stmt.executeQuery(sqlString);
-            while (rset.next()){
-                xPos = rset.getInt("XPOS");
-                yPos = rset.getInt("YPOS");
-                hiddenString = rset.getString("HIDDEN");
-                hidden = hiddenString.equals("Y");
-                name = rset.getString("name");
-                nodes.add(new Node(xPos, yPos, hidden, name, floor));
-            }
-            stmt.close();
-        } catch (SQLException se) {
-            se.printStackTrace();
-        }
-        return nodes;
-    }
-    */
-
-    public ResultSet getNodeSet(){
-        String sqlString = "Select * FROM NODE";
-
-        ResultSet rset = null;
-        try {
-            Statement stmt = conn.createStatement();
-            rset = stmt.executeQuery(sqlString);
+            resultSet = stmt.executeQuery(sqlString);
             stmt.close();
         } catch (SQLException se) {
             se.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return rset;
+        return resultSet;
     }
 
     // creates a new node in the database
-    public void newNode(int x, int y, boolean hidden, String name,int floor){
-        System.out.println("Adding node");
-        String hiddenString;
-        if (hidden) {
-            hiddenString = "Y";
-        } else {
-            hiddenString = "N";
-        }
+    public boolean newNode(int x, int y, int floor, boolean ishidden, boolean enabled,
+                           String type, String name, String roomnum){
+        System.out.println(
+                String.format(
+                        "Adding node. x: %s, y: %s, hidden: %s, name: %s, floor: %s",
+                            x, y, ishidden, name, floor));
         try {
             // sql statement with "?" to be filled later
-            String query = "INSERT INTO NODE (YPOS, XPOS, NAME, FLOOR, ISHIDDEN)" +
-                    " values (?, ?, ?, ?, ?)";
+            String query = "INSERT INTO NODE (XPOS, YPOS, FLOOR, ISHIDDEN, ENABLED, TYPE, NAME, ROOMNUM)" +
+                    " values (?, ?, ?, ?, ?, ?, ?, ?)";
             // prepare statement by replacing "?" with corresponding variable
             PreparedStatement preparedStatement = conn.prepareStatement(query);
             preparedStatement.setInt(1, y);
             preparedStatement.setInt(2, x);
-            preparedStatement.setString(3, name);
-            preparedStatement.setInt(4, floor);
-            preparedStatement.setString(5, hiddenString);
+            preparedStatement.setInt(3, floor);
+            preparedStatement.setBoolean(4, ishidden);
+            preparedStatement.setBoolean(5, enabled);
+            preparedStatement.setString(6, type);
+            preparedStatement.setString(7, name);
+            preparedStatement.setString(8, roomnum);
             // execute prepared statement
             preparedStatement.execute();
             preparedStatement.close();
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
-        System.out.println("Node added");
-    }
-
-    // creates a new edge in the database
-    public void newEdge(int xPos1, int yPos1, int floor1, int xPos2, int yPos2, int floor2){
-        try {
-            // sql statement with "?" to be filled later
-            String query = "INSERT INTO EDGE (XPOS1, YPOS1, XPOS2, YPOS2, FLOOR1, FLOOR2)" +
-                    " values (?, ?, ?, ?, ?, ?)";
-            // prepare statement by replacing "?" with corresponding variable
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setInt(1, xPos1);
-            preparedStatement.setInt(2, yPos1);
-            preparedStatement.setInt(3, xPos2);
-            preparedStatement.setInt(4, yPos2);
-            preparedStatement.setInt(5, floor1);
-            preparedStatement.setInt(6, floor2);
-            // execute prepared statement
-            preparedStatement.execute();
-            preparedStatement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        return true;
     }
 
     // finds the node with the given info and edits it
     // first deletes the nodes, then creates another one with the given information
-    public void EditNode(int yPos, int xPos, int floor, boolean hidden, String name){
+    public boolean EditNode(int x, int y, int floor, boolean ishidden, boolean enabled,
+                         String type, String name, String roomnum){
         // first we delete the node, because we don't want to change its primary keys
-        deleteNode(xPos, yPos, floor);
+        if(!deleteNode(x, y, floor)){
+            return false;
+        }
         // then we create a new node with the old's one info
-        newNode(xPos, yPos, hidden, name, floor);
+        if(!newNode(x, y, floor, ishidden, enabled, type, name, roomnum)){
+            return false;
+        }
+
+        return true;
     }
 
-    //delete edge between the two given node positions
-    public void deleteEdge(int xPos1, int yPos1, int floor1, int xPos2, int yPos2, int floor2) {
-        try {
-            // SQL statement with "?" to be filled later
-            String sqlString = "DELETE FROM EDGE WHERE XPOS1 = ? AND YPOS1 = ?" +
-                    "AND FLOOR1 = ? AND XPOS2 = ? AND YPOS2 = ? AND FLOOR2 = ?";
-            // prepare statement by replacing each "?" with a variable
-            PreparedStatement preparedStatement = conn.prepareStatement(sqlString);
-            preparedStatement.setInt(1, xPos1);
-            preparedStatement.setInt(2, yPos1);
-            preparedStatement.setInt(3, floor1);
-            preparedStatement.setInt(4, xPos2);
-            preparedStatement.setInt(5, yPos2);
-            preparedStatement.setInt(6, floor2);
-            // run statement and query
-            preparedStatement.execute();
-            preparedStatement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
     //delete node given its position
-    public void deleteNode(int xPos, int yPos, int floor){
+    public boolean deleteNode(int x, int y, int floor){
         try {
             // SQL statement with "?" to be filled later
             String query = "DELETE FROM NODE WHERE XPOS = ? AND YPOS = ? AND FLOOR = ?";
             // prepare statement by replacing each "?" with a variable
             PreparedStatement preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setInt(1, xPos);
-            preparedStatement.setInt(2, yPos);
+            preparedStatement.setInt(1, x);
+            preparedStatement.setInt(2, y);
             preparedStatement.setInt(3, floor);
             // run statement and query
             preparedStatement.execute();
             preparedStatement.close();
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
+        return true;
+    }
+
+    // creates a new edge in the database
+    public boolean newEdge(int x1, int y1, int floor1, int x2, int y2, int floor2){
+        try {
+            // sql statement with "?" to be filled later
+            String query = "INSERT INTO EDGE (XPOS1, YPOS1, FLOOR1, XPOS2, YPOS2, FLOOR2)" +
+                    " values (?, ?, ?, ?, ?, ?)";
+            // prepare statement by replacing "?" with corresponding variable
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, x1);
+            preparedStatement.setInt(2, y1);
+            preparedStatement.setInt(3, floor1);
+            preparedStatement.setInt(4, x2);
+            preparedStatement.setInt(5, y2);
+            preparedStatement.setInt(6, floor2);
+            // execute prepared statement
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    //delete edge between the two given node positions
+    public boolean deleteEdge(int x1, int y1, int floor1, int x2, int y2, int floor2) {
+        try {
+            // SQL statement with "?" to be filled later
+            String sqlString = "DELETE FROM EDGE WHERE XPOS1 = ? AND YPOS1 = ?" +
+                    "AND FLOOR1 = ? AND XPOS2 = ? AND YPOS2 = ? AND FLOOR2 = ?";
+            // prepare statement by replacing each "?" with a variable
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlString);
+            preparedStatement.setInt(1, x1);
+            preparedStatement.setInt(2, y1);
+            preparedStatement.setInt(3, floor1);
+            preparedStatement.setInt(4, x2);
+            preparedStatement.setInt(5, y2);
+            preparedStatement.setInt(6, floor2);
+            // run statement and query
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean newProfessional(String ID, int x, int y, int floor,
+                                   String firstName, String lastName, String type){
+        try{
+            // sql statement with "?" to be filled later
+            String query = "INSERT INTO PROFESSIONAL (ID, XPOS, YPOS, FLOOR, FIRSTNAME, LASTNAME, TYPE)" +
+                    " values (?, ?, ?, ?, ?, ?, ?)";
+            // prepare statement by replacing "?" with corresponding variable
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, ID);
+            preparedStatement.setInt(2, x);
+            preparedStatement.setInt(3, y);
+            preparedStatement.setInt(4, floor);
+            preparedStatement.setString(5, firstName);
+            preparedStatement.setString(6, lastName);
+            preparedStatement.setString(7, type);
+            // execute prepared statement
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    // finds the node with the given info and edits it
+    // first deletes the nodes, then creates another one with the given information
+    public boolean EditProfessional(String ID, int x, int y, int floor,
+                                    String firstName, String lastName, String type){
+        // first we delete the node, because we don't want to change its primary keys
+        if(!deleteProfessional(ID)){
+            return false;
+        }
+        // then we create a new node with the old's one info
+        if(!newProfessional(ID, x, y, floor, firstName, lastName, type)){
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean deleteProfessional(String ID){
+        try{
+            String sqlString = "DELTE FROM PROFESSIONAL WHERE ID = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlString);
+            preparedStatement.setString(1, ID);
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
