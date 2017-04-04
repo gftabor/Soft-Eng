28c2ab55410e837f1a03ed1fbb6ca45903ea1b8c
Ryan Walsh <rpwalsh@wpi.edu>
2017-04-04 17:45:37 -0400

    Finished adding full source code comments to application - where needed. This commit added a few final lines to DatabaseController and to AdminLoginManager

--- src/main/java/adminLoginMain/AdminLoginManager.java
+++ src/main/java/adminLoginMain/AdminLoginManager.java
@@ -8,6 +8,7 @@ public class AdminLoginManager {
     private int loginAttempts;
     private String hashed1234 = "$2a$10$MyXFwAYccDvY6B8zXDYDrOldaF0kBrPI/ybyUXCOhC0.i7o7qaCBu";
 
+    //uses BCrypt hash function to check password encryption
     public int verifyCredentials(String username, String password) {
         if((username.equals("Griffin")) && (BCrypt.checkpw(password, hashed1234))){
             return 1;
