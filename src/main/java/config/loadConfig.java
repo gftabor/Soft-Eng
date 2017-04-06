d200c0f6eea7b5b967459ccdbd5f1b738a50c55c
Jason Ashton <jlashton@wpi.edu>
2017-04-06 14:11:52 -0400

    Added functionality to both read from a config as well as send an sms

---
+++ src/main/java/config/loadConfig.java
@@ -0,0 +1,46 @@
+package config;
+import com.eclipsesource.json.*;
+import java.io.*;
+
+/**
+ * Created by jasonashton on 4/6/17.
+ */
+public class loadConfig {
+    Reader reader;
+    JsonObject auth;
+
+    public loadConfig() {
+        reader = startReader();
+        try{
+            auth = Json.parse(reader).asObject();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getTwilioAuth(){
+        return auth.get("accountID").asString();
+    }
+
+    public String getTwilioPW(){
+        return auth.get("accountPW").asString();
+    }
+
+    public String getTwilioSendTo(){
+        return auth.get("sendto").asString();
+    }
+
+    public String getTwilioSendFrom(){
+        return auth.get("sendfrom").asString();
+    }
+
+    private Reader startReader(){
+        Reader reader = null;
+        try {
+            reader = new FileReader("/Users/jasonashton/IdeaProjects/Soft-Eng/build/resources/main/config/config.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return reader;
+    }
+}
---
+++ src/main/java/emergency/SmsSender.java
@@ -0,0 +1,33 @@
+package emergency;
+
+// Install the Java helper library from twilio.com/docs/java/install
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import config.loadConfig;
+
+
+import java.net.URISyntaxException;
+
+/**
+ * Created by jasonashton on 4/6/17.
+ */
+
+public class SmsSender {
+
+    // Find your Account Sid and Auth Token at twilio.com/console
+    loadConfig config = new loadConfig();
+
+
+    public void sendSMS() throws URISyntaxException {
+        Twilio.init(config.getTwilioAuth(), config.getTwilioPW());
+
+        Message message = Message
+                .creator(new PhoneNumber(config.getTwilioSendTo()),  // to
+                        new PhoneNumber(config.getTwilioSendFrom()),  // from
+                        "EMERGENCY - VISIT KIOSK")
+                .create();
+        System.out.println("Message SID: " + message.getSid());
+        System.out.println("Message Status: " + message.getStatus());
+    }
+}
