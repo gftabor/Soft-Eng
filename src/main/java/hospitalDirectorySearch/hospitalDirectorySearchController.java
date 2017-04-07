52b14fe5998e784bb3932859d70837d1deaea12f
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-06 23:02:38 -0400

    Added hospital directory for the user without features

--- src/main/java/hospitalDirectorySearch/hospitalDirectorySearchController.java
+++ src/main/java/hospitalDirectorySearch/hospitalDirectorySearchController.java
@@ -1,5 +1,9 @@
 package hospitalDirectorySearch;
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import DBController.DatabaseController;
 import controllers.Professional;
@@ -9,6 +13,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+
+
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 
@@ -23,10 +31,98 @@ import java.util.regex.Pattern;
 /**
  * Created by AugustoR on 4/6/17.
  */
-public class hospitalDirectorySearchController {
+public class hospitalDirectorySearchController extends controllers.AbsController{
+
     @FXML
     private AnchorPane backgroundAnchorPane;
 
+    @FXML
+    private Button mainMenu_Button;
+
+    @FXML
+    private Button emergency_Button;
+
+    @FXML
+    private TextField serach_TextField;
+
+    @FXML
+    private TextField from_TextField;
+
+    @FXML
+    private TextField to_TextField;
+
+    @FXML
+    private Button clear_Button;
+
+    @FXML
+    private Button submit_Button;
+
+    @FXML
+    private Label currentAdmin_Label;
+
+    @FXML
+    private TableView<Table> Table_TableView;
+
+    @FXML
+    private TableColumn<Table, Integer> ID_TableColumn;
+
+    @FXML
+    private TableColumn<Table, String> firstName_TableColumn;
+
+    @FXML
+    private TableColumn<Table, String> lastName_TableColumn;
+
+    @FXML
+    private TableColumn<Table, String> title_TableColumn;
+
+    @FXML
+    private TableColumn<Table, String> department_TableColumn;
+
+    @FXML
+    private TableColumn<Table, String> room_TableColumn;
+
+    int iNumber = 1; 
+
+
+    //
+    public void mainMenuButton_Clicked(){
+        FXMLLoader loader = switch_screen(backgroundAnchorPane, "/views/patientMenuStartView.fxml");
+        adminMenuStart.adminMenuStartController controller = loader.getController();
+        //Set the correct username for the next scene
+        controller.setUsername(currentAdmin_Label.getText());
+    }
+
+    //
+    public void emergencyButton_Clicked(){
+        switch_screen(backgroundAnchorPane, "/views/emergencyView.fxml");
+    }
+
+    //
+    public void clearButton_Clicked(){
+        System.out.println("The user has clicked the clear button");
+    }
+
+    //
+    public void submitButton_Clicked(){
+        System.out.println("The user has clicked the submit button");
+    }
+
+
+
+   final ObservableList<Table> data = FXCollections.observableArrayList();
+    //Sets the table
+    public void setTable(){
+        ID_TableColumn.setCellValueFactory(new PropertyValueFactory<Table, Integer>("rID"));
+        firstName_TableColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("rFirstName"));
+        lastName_TableColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("rLastName"));
+        title_TableColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("rTitle"));
+        department_TableColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("rDepartment"));
+        room_TableColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("rRoom"));
+
+        Table_TableView.setItems(data);
+
+
+    }
 
 
 }
