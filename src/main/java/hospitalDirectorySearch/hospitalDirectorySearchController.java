2f8065c2f5e20105845377b7097f7a474b57ec7a
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-06 21:01:53 -0400

    added the new scene called hospitalDirectorySearchController

---
+++ src/main/java/hospitalDirectorySearch/hospitalDirectorySearchController.java
@@ -0,0 +1,32 @@
+package hospitalDirectorySearch;
+
+import javafx.scene.layout.AnchorPane;
+import DBController.DatabaseController;
+import controllers.Professional;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.event.KeyEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * Created by AugustoR on 4/6/17.
+ */
+public class hospitalDirectorySearchController {
+    @FXML
+    private AnchorPane backgroundAnchorPane;
+
+
+
+}
