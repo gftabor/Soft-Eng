c1a2ad42ea783e82e36b03a2361daf613253de20
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-08 17:08:36 -0400

    WORKING DOCUMENT

--- src/main/java/adminMenuStart/adminMenuStartController.java
+++ src/main/java/adminMenuStart/adminMenuStartController.java
@@ -65,13 +65,13 @@ public class adminMenuStartController extends controllers.AbsController{
     public void hospitalDirectoryButton_Clicked(){
          FXMLLoader loader= switch_screen(backgroundAnchorPane, "/views/mmNodeInformationView.fxml");
          mapManagementNodeInformation.mmNodeInformationController controller = loader.getController();
-         // controller.createDirectoryTreeView();
+
          //controller.setTitleChoices();
          //controller.setModeChoices();
          //controller.setRoomChoices();
          //controller.setDepartmentChoices();
-         //controller.setUser(username_Label.getText());
-         //controller.setUser(username_Label.getText());
+        // controller.setUser(username_Label.getText());
+
 
 
     }
