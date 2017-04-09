d87d4125cdf40bff3fc6297aec14171edaa152a3
Jason Ashton <jlashton@wpi.edu>
2017-04-09 17:30:10 -0400

    Fixed class name for databaseGeneratorAbstract

--- src/main/java/DBController/databaseGeneratorAbstract.java
+++ src/main/java/DBController/databaseGeneratorAbstract.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 /**
  * Created by jasonashton on 4/8/17.
  */
-abstract class testDatabaseAbstract {
+abstract class databaseGeneratorAbstract {
     private Connection conn = null;
     private Statement stmt = null;
 
