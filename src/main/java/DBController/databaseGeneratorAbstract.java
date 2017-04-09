d305ebb20c02f3cd835fd049603166b93cbe9e1a
Jason Ashton <jlashton@wpi.edu>
2017-04-08 21:42:17 -0400

    Separated the database generators into two subclasses of an abstract class

---
+++ src/main/java/DBController/databaseGeneratorAbstract.java
@@ -0,0 +1,76 @@
+package DBController;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by jasonashton on 4/8/17.
+ */
+abstract class testDatabaseAbstract {
+    private Connection conn = null;
+    private Statement stmt = null;
+
+    public boolean connectDB() {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Java DB Driver not found. Add the classpath to your module.");
+            e.printStackTrace();
+            return false;
+        }
+
+
+        try {
+            conn = DriverManager.getConnection("jdbc:derby:testDB;create=true");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean populateDB(){
+        return readSQL("build/resources/main/database/testDatabaseSetup.sql");
+    }
+
+    public boolean clearDB(){
+        return readSQL("build/resources/main/database/testDatabaseClear.sql");
+    }
+
+    private boolean readSQL(String path){
+        String s = new String();
+        StringBuffer sb = new StringBuffer();
+
+        try {
+            FileReader fr = new FileReader(new File(path));
+
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((s = br.readLine()) != null) {
+                sb.append(s);
+            }
+            br.close();
+
+            String[] inst = sb.toString().split(";");
+
+            stmt = conn.createStatement();
+
+            for (int i = 0; i < inst.length; i++) {
+                if (!inst[i].trim().equals("")) {
+                    stmt.executeUpdate(inst[i]);
+                    System.out.println(">>" + inst[i]);
+                }
+            }
+            stmt.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ src/main/java/DBController/mainDatabaseGenerator.java
@@ -0,0 +1,12 @@
+package DBController;
+
+/**
+ * Created by jasonashton on 4/8/17.
+ */
+public class mainDatabaseGenerator {
+    String dbName = "FaulknerDB";
+
+    public String getDbName(){
+        return dbName;
+    }
+}
