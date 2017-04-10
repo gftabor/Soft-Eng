08cad408a21109a167f0a6300ea88ea266ebb750
Jason Ashton <jlashton@wpi.edu>
2017-04-10 10:09:07 -0400

    Changed test database controller to be a static instance

--- src/test/java/database/testTestDB.java
+++ src/test/java/database/testTestDB.java
@@ -11,7 +11,7 @@ import static junit.framework.TestCase.*;
  */
 public class testTestDB {
 
-    testDatabaseController testDBController = new testDatabaseController();
+    testDatabaseController testDBController = testDatabaseController.getInstance();
 
     @Before
     public void startDB(){
