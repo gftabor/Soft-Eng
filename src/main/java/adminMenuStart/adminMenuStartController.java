b325a2cc608f7f34794df8f2fafaaa883bf88f13
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-10 16:53:18 -0400

    added more english/spanish navigation pathfinding adminmenu and login

--- src/main/java/adminMenuStart/adminMenuStartController.java
+++ src/main/java/adminMenuStart/adminMenuStartController.java
@@ -34,6 +34,9 @@ public class adminMenuStartController extends controllers.AbsController{
     @FXML
     private Button mapManagement_Button;
 
+    @FXML
+    private Label title_Label;
+
     //Set to english by default
     int c_language = 0;
 
@@ -42,7 +45,17 @@ public class adminMenuStartController extends controllers.AbsController{
         System.out.println("The user has clicked the sign out button");
 
         //Change to patient menu
-        switch_screen(backgroundAnchorPane, "/views/patientMenuStartView.fxml");
+        FXMLLoader loader= switch_screen(backgroundAnchorPane, "/views/patientMenuStartView.fxml");
+        patientMenuStart.patientMenuStartController controller = loader.getController();
+        //sends the current language to the next screen
+        controller.setCurrentLanguage(c_language);
+        //set up english labels
+        if(c_language == 0){
+            controller.englishButtons_Labels();
+            //set up spanish labels
+        }else if(c_language == 1){
+            controller.spanishButtons_Labels();
+        }
 
     }
 
@@ -74,6 +87,18 @@ public class adminMenuStartController extends controllers.AbsController{
         //controller.setMapAndNodes(DBMap);
         controller.setUserString(username_Label.getText());
 
+        //sends the current language to the next screen
+        controller.setC_language(c_language);
+        //set up english labels
+        if(c_language == 0){
+            controller.englishButtons_Labels();
+            //set up spanish labels
+        }else if(c_language == 1){
+            controller.spanishButtons_Labels();
+        }
+
+
+
     }
 
     public void hospitalDirectoryButton_Clicked(){
@@ -99,6 +124,7 @@ public class adminMenuStartController extends controllers.AbsController{
         mapManagementFloorAndMode.mmFloorAndModeController controller = loader.getController();
         controller.setUserString(username_Label.getText());
 
+
         //Set the username label
     }
 
@@ -110,6 +136,46 @@ public class adminMenuStartController extends controllers.AbsController{
     //Set the username coming from a scene
     public void setUsername(String user){
         username_Label.setText(user);
+    }
+
+
+    //switches all the labels and Buttons to english
+    public void englishButtons_Labels(){
+        //change the current language to english
+        c_language = 0;
+
+        //Change the Buttons
+        pathFinding_Button.setText("Map");
+        hospitalDirectory_Button.setText("Directory Management");
+        mapManagement_Button.setText("Map Management");
+        SignOut_Button.setText("Sign Out");
+        emergency_Button.setText("EMERGENCY");
+
+        //Change the labels
+        title_Label.setText("Welcome to Faulkner Hospital");
+    }
+
+    //switches all teh labels to spanish
+    public void spanishButtons_Labels() {
+        //change the current language to spanish
+        c_language = 1;
+
+        //change the Buttons
+        pathFinding_Button.setText("Mapa");
+        hospitalDirectory_Button.setText("Control de Directorio");
+        mapManagement_Button.setText("Control de Mapa");
+        SignOut_Button.setText("Salir");
+        emergency_Button.setText("EMERGENCIA");
+
+        //change the Labels
+        title_Label.setText("Bienvenidos al Hospital de Faulkner");
+
+
+    }
+
+    //sets the current language given information form other screens
+    public void setCurrentLanguage(int i){
+        c_language = i;
     }
 
 
