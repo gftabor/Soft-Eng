a40381cc308553193518a37c06753ae4dd4bbdb7
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-21 18:46:24 -0400

    had fun playing with the new scene

--- src/main/java/NewIntroUI/NewIntroUIController.java
+++ src/main/java/NewIntroUI/NewIntroUIController.java
@@ -1,5 +1,7 @@
 package NewIntroUI;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXML;
 
@@ -9,5 +11,44 @@ import javafx.fxml.FXML;
 public class NewIntroUIController {
     @FXML
     private AnchorPane backgroundAnchorPane;
+
+    @FXML
+    private Button minus_Button;
+
+    @FXML
+    private Button plus_Button;
+
+    @FXML
+    private ToggleButton hiContrast_ToggleButton;
+
+    @FXML
+    private ToggleButton largeUI_ToggleButton;
+
+    public void minusButton_Clicked(){
+
+    }
+
+    public void plusButton_Clicked(){
+
+    }
+
+    public void hiContrastButton_Clicked(){
+        if (hiContrast_ToggleButton.isSelected() == true) {
+            backgroundAnchorPane.getStyleClass().add("contrast");
+        } else {
+            backgroundAnchorPane.getStyleClass().remove("contrast");
+        }
+
+    }
+
+    public void largeUIButton_Clicked(){
+        if (largeUI_ToggleButton.isSelected() == true) {
+            backgroundAnchorPane.getStyleClass().add("touch-sizes");
+        } else {
+            backgroundAnchorPane.getStyleClass().remove("touch-sizes");
+        }
+
+    }
+
 
 }
