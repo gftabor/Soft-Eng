8ca7c4d5e3a5a345055f1a7d90f8357d519fd599
Augusto Rolando Wong Quan <arwong@wpi.edu>
2017-04-21 17:58:22 -0400

    Working on the NEW UI, Started with a new view (TESTING)

---
+++ src/main/java/NewIntroUI/NewIntroUIController.java
@@ -0,0 +1,13 @@
+package NewIntroUI;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXML;
+
+/**
+ * Created by AugustoR on 4/21/17.
+ */
+public class NewIntroUIController {
+    @FXML
+    private AnchorPane backgroundAnchorPane;
+
+}
