67356e47d6b35c997123ea7f899ea6bb09421a71
Griffin Cecil <gdcecil@wpi.edu>
2017-04-24 20:02:39 -0400

    Implemented separate buttons for zoom in and out to be put on map, mouse wheel now zooms in and out, set up for full screen display

--- src/main/java/controllers/MapOverlay.java
+++ src/main/java/controllers/MapOverlay.java
@@ -32,6 +32,8 @@ public class MapOverlay {
     private ArrayList<Line> lineList = new ArrayList<Line>();
 
     private static double zoom = 1;
+    private static double heightRatio = 1;
+    private static double widthRatio = 1;
 
     private static int pathfinding = 0;
 
@@ -105,8 +107,8 @@ public class MapOverlay {
 
         // this code sets node's x and y pos to be on the plane holding the graph
         currentPane.getChildren().add(location);
-        location.setLayoutX(nodeX * zoom);
-        location.setLayoutY(nodeY * zoom);
+        location.setLayoutX(nodeX * zoom * widthRatio);
+        location.setLayoutY(nodeY * zoom * heightRatio);
         location.toFront();
 
         if (!enabled) {
@@ -153,10 +155,10 @@ public class MapOverlay {
 
             //add to pane
             currentPane.getChildren().add(lne);
-            lne.setStartX(thisEdge.getStartNode().getPosX() * zoom);
-            lne.setStartY(thisEdge.getStartNode().getPosY() * zoom);
-            lne.setEndX(thisEdge.getEndNode().getPosX() * zoom);
-            lne.setEndY(thisEdge.getEndNode().getPosY() * zoom);
+            lne.setStartX(thisEdge.getStartNode().getPosX() * zoom * widthRatio);
+            lne.setStartY(thisEdge.getStartNode().getPosY() * zoom * heightRatio);
+            lne.setEndX(thisEdge.getEndNode().getPosX() * zoom * widthRatio);
+            lne.setEndY(thisEdge.getEndNode().getPosY() * zoom * heightRatio);
             //show
             lne.toFront();
             //add to list
@@ -202,5 +204,21 @@ public class MapOverlay {
 
     public static void setPathfinding(int newpath) {
         pathfinding = newpath;
+    }
+
+    public static void setHeightRatio(double ratio) {
+         heightRatio = ratio;
+    }
+
+    public static double getHeightRatio() {
+        return heightRatio;
+    }
+
+    public static void setWidthRatio(double ratio) {
+        widthRatio = ratio;
+    }
+
+    public static double getWidthRatio() {
+        return widthRatio;
     }
 }
