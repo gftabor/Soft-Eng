b31d751f1f4445eab61c1741b3f04b4c6f2f60d5
Ryan Walsh <rpwalsh@wpi.edu>
2017-04-27 15:46:33 -0400

    Editor workflow updated. Can now click on map and not have immediate popups

--- src/main/java/mapManagementFloorAndMode/mmFloorAndModeController.java
+++ src/main/java/mapManagementFloorAndMode/mmFloorAndModeController.java
@@ -131,10 +131,12 @@ public class mmFloorAndModeController extends controllers.mapScene{
     private boolean addSingleEdgeMode;
     private boolean addMultiEdgeMode;
     private boolean dragMode;
+    private boolean popoverShown;
 
     private int permissionLevel;
 
     final Circle[] temporaryButton = {null};
+
 
 
     //Set to english by default
@@ -152,8 +154,7 @@ public class mmFloorAndModeController extends controllers.mapScene{
         setTitleChoices();
         addSingleEdgeMode = false;
         addMultiEdgeMode = false;
-
-        final Circle[] temporaryButton = {null};
+        popoverShown = false;
 
         selectedNode = false;
 
@@ -187,10 +188,15 @@ public class mmFloorAndModeController extends controllers.mapScene{
                 selectedNode = false;
                 graph.wipeEdgeLines();
                 //color the node as well
-                if (lastColoredStart !=  null) {
+                if (lastColoredStart != null) {
                     lastColoredStart.setStroke(lastColoredStart.getFill());
                     lastColoredStart.setStrokeWidth(1);
                     lastColoredStart = null;
+                }
+            } else if(popoverShown) {
+                popoverShown = false;
+                if (temporaryButton[0] != null && !databaseController.isActualLocation((int) temporaryButton[0].getLayoutX(), (int) temporaryButton[0].getLayoutY(), currentFloor)){
+                    admin_FloorPane.getChildren().remove(temporaryButton[0]);
                 }
             } else {
                 graph.wipeEdgeLines();
@@ -203,6 +209,9 @@ public class mmFloorAndModeController extends controllers.mapScene{
                 btK.setLayoutY(e.getY());
                 admin_FloorPane.getChildren().add(btK);
                 temporaryButton[0] = btK;
+
+                //set the popovershown var
+                popoverShown = true;
 
                 PopOver pop = new PopOver();
                 createPop(pop, btK, "Create");
@@ -679,8 +688,11 @@ public class mmFloorAndModeController extends controllers.mapScene{
         selectedNode = true;
 
         //remove any temporary nodes
-        if (temporaryButton[0] != null && !databaseController.isActualLocation((int) temporaryButton[0].getLayoutX(), (int) temporaryButton[0].getLayoutY(), currentFloor)){
-            admin_FloorPane.getChildren().remove(temporaryButton[0]);
+        if (popoverShown){
+            if (temporaryButton[0] != null && !databaseController.isActualLocation((int) temporaryButton[0].getLayoutX(), (int) temporaryButton[0].getLayoutY(), currentFloor)){
+                admin_FloorPane.getChildren().remove(temporaryButton[0]);
+            }
+
         }
         //highlight the node
         nodeEdgeX1 = (int) x;
